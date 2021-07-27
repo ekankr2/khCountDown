d0c4b23cdcf9e3d6efcae201d2d5d67fdf95d2d1
ekank <ekankr2@naver.com>
2021-07-27 22:54:35 +0900

    vue weather app

---
+++ vueweather/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search...">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      api_key: '6d523938fc6d2de1fe87a72c783a4d33'
+    }
+  }
+
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'montserrat',sans-serif;
+}
+
+#app{
+  background-image: url("assets/cold-bg.jpg");
+}
+</style>
