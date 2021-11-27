de49de9224a70923b069871e53d5a9dfc4001ebd
ekank <ekankr2@naver.com>
2021-11-27 21:07:41 +0900

    lotter numbers function

--- lotto/src/App.js
+++ lotto/src/App.js
@@ -7,16 +7,45 @@ function App() {
     const heroku = 'https://esc-proxy.herokuapp.com/'
     const request = 'https://www.dhlottery.co.kr/'
 
-  const getLotto = () =>{
+    const getLotto = () =>{
       axios.get(`${heroku}${request}common.do?method=getLottoNumber&drwNo=861`)
           .then((result)=>{
             console.log(result.data)
           })
-  }
+    }
+
+    function getRandomInt() {
+        return Math.floor(Math.random() * 45) + 1 //최댓값은 제외, 최솟값은 포함
+    }
+
+    function sixRandomInt() {
+        let luckyNumbers = []
+
+        while (true){
+            let randomInt = getRandomInt()
+
+            if(!luckyNumbers.includes(randomInt)){
+                luckyNumbers.push(randomInt)
+            }
+            if(luckyNumbers.length === 6){
+                break
+            }
+        }
+        return luckyNumbers
+    }
+
+    function sortLottery(){
+        let sortArray = sixRandomInt()
+
+        sortArray.sort((a, b)=>{
+            return a - b;
+        })
+        return sortArray
+    }
 
   return (
     <>
-      <button onClick={getLotto}>클릭</button>
+      <button onClick={()=>{console.log(sortLottery())}}>클릭</button>
     </>
   );
 }
